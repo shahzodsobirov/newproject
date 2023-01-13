9b94f4489a8eacc0fb9110c643df05760e974a86
shahzodsobirov <shahzodsobirov246@gmail.com>
2023-01-13 10:28:55 +0500

    defe

--- static/JS/creat_test.js
+++ static/JS/creat_test.js
@@ -3,31 +3,31 @@ const box = document.querySelectorAll('.variant');
 const active = document.querySelectorAll('.controll_active');
 box[0].style.display = 'block'
 
-// function removeDiv() {
-//     box.forEach(item => {
-//         item.style.display = 'none'
-//     })
-// }
-//
-// function addLink() {
-//     link.forEach((item, index) => {
-//         item.addEventListener('click', () => {
-//             // console.log(index)
-//             removeLink();
-//             removeDiv()
-//             item.classList.add('active');
-//             box[index].style.display = 'flex'
-//         });
-//     });
-// }
-//
-// addLink();
-//
-// function removeLink() {
-//     link.forEach(item => {
-//         item.classList.remove('active');
-//     });
-// }
+function removeDiv() {
+    box.forEach(item => {
+        item.style.display = 'none'
+    })
+}
+
+function addLink() {
+    link.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            // console.log(index)
+            removeLink();
+            removeDiv()
+            item.classList.add('active');
+            box[index].style.display = 'flex'
+        });
+    });
+}
+
+addLink();
+
+function removeLink() {
+    link.forEach(item => {
+        item.classList.remove('active');
+    });
+}
 
 const block = document.querySelector('.block');
 
@@ -92,14 +92,10 @@ function catchInputChange() {
                     }
                     return variant
                 })
-
                 return {...item,variants: newVariants}
             })
-
-
         })
     })
-
 }
 
 function catchCheckboxChange() {
@@ -124,90 +120,6 @@ function catchCheckboxChange() {
     })
 }
 
-// function question_info() {
-//     inp.forEach((item, index) => {
-//         item.addEventListener("input", () => {
-//             // let subject_id = sub.value;
-//             // let level_id = lev.value
-//             // console.log(subject_id)
-//             let info = {
-//                 "id": index + 1,
-//                 "question": question.value,
-//                 "variant": [
-//                     {
-//                         variant: "a"
-//                     },
-//                     {
-//                         variant: "b"
-//                     },
-//                     {
-//                         variant: "c"
-//                     },
-//                     {
-//                         variant: "d"
-//                     }
-//                 ]
-//             }
-//             question_list.push(info)
-//         })
-//     })
-// }
-
-
-
-// function question_info(input, type) {
-//     input.forEach((item, index) => {
-//         item.addEventListener("input", () => {
-//             let info = {
-//                 "id": index + 1, "question": question.value,
-//                 "variant": [{
-//                     "inp": "", "inp2": "", "inp3": "", "inp4": ""
-//                 }
-//                 ],
-//                 variants: [
-//                     {
-//                         name : "variant1",
-//                         value : "",
-//                         checked : false
-//                     },
-//                 ]
-//
-//             }
-//             {
-//                 info.variant[`${type}`] = item.value
-//             }
-//             // console.log(info)
-//             question_list.push(info)
-//         })
-//     })
-// }
-//
-// console.log(question_list)
-// question_info(inp, "inp")
-// question_info(inp2, "inp2")
-// question_info(inp3, "inp3")
-// question_info(inp4, "inp4")
-//
-// function check() {
-//     checkboxes.forEach((item, index) => {
-//         item.addEventListener("change", () => {
-//             if (item.checked) {
-//                 question_list = question_list.map((text, i) => {
-//                     if (text.id === index + 1) {
-//                         return {...text, checked: true}
-//                     }
-//                     return text
-//                 })
-//             }
-//             console.log(question_list)
-//         })
-//     })
-// }
-//
-// check()
-// console.log(question_list)
-
-
 let variant = document.querySelector(".variant"), plus2 = document.querySelector(".plus2");
 plus2.addEventListener("click", () => {
     const newItem = {
@@ -218,12 +130,6 @@ plus2.addEventListener("click", () => {
 
     question_list[0].variants.push(newItem)
     renderVariants(question_list)
-    // variant.innerHTML += `
-    //         <div class="inp">
-    //             <h4>1)</h4>
-    //             <input type="text" placeholder="Enter your variant" name="variant" class="inp2">
-    //             <input type="checkbox" data-id="1" class="checkbox">
-    //         </div>`
 })
 
 
